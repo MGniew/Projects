0a1e15a5e8f0a874133e0b26e53b8b0d3814828c
MGniew <218138@student.pwr.edu.pl>
2018-02-21 14:29:21 +0100

    Numbers/Tile

---
+++ SOLUTIONS/Python/Numbers/tile.py
@@ -0,0 +1,10 @@
+def main():
+
+	width = float(input("What is the width of the floor?: "))
+	length = float(input("What is the length of the floor?: "))
+	cost = float(input("Cost: "))
+
+	print(width * length * cost)
+
+if __name__ == "__main__":
+	main()
